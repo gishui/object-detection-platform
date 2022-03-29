32afde8639b52a2bd6725ec85334615cfb1938c9
Tamashi_Hu <1730558797@qq.com>
2022-03-29 11:51:58 +0800

    解决后端模型类别名称问题和前端消息提示优化

---
+++ back-end/dota_name.py
@@ -0,0 +1,18 @@
+id2name3 = {
+         0:'small-vehicle',
+         1:'large-vehicle',
+         2:'plane',
+         3:'storage-tank',
+         4:'ship',
+         5:'harbor',
+         6:'ground-track-field',
+         7:'soccer-ball-field',
+         8:'tennis-court',
+         9:'swimming-pool',
+         10:'baseball-diamond',
+         11:'roundabout',
+         12:'basketball-court',
+         13:'bridge',
+         14:'helicopter',
+         15:'container-crane'
+    }
